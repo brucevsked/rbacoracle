7ae4b447a7a9f497ae350e124f51efeea7418989
chousheng <vsked@163.com>
2018-03-02 09:07:23 +0800

    fixed sql for oracle

--- documents/sqlscript/bussdatainit.sql
+++ documents/sqlscript/bussdatainit.sql
@@ -23,27 +23,35 @@
 /**  数据查询区  **/
 
 /********+*********+*********+*********+*********+*********+*/
-drop table carrierT;
+drop table "carrierT";
 
 /********+*********+*********+*********+*********+*********+*/
 
-create table carrierT(
-carrierId varchar(64)  primary key                COMMENT '编号'    ,
-carrierName varchar(64) unique not null           COMMENT '运营商名',
-carrierNick varchar(64)                           COMMENT '别名'    ,
-carrierShortName varchar(64)                      COMMENT '简称号'  ,
-officialWebSite varchar(256)                      COMMENT '官网'    ,
-hotline  varchar(64)                              COMMENT '客服电话',
-carrierAddTime timestamp default CURRENT_TIMESTAMP  COMMENT '添加时间'
+create table "carrierT"(
+"carrierId" nvarchar2(64)  primary key        , -- 编号
+"carrierName" nvarchar2(64) unique not null   , -- 运营商名
+carrierNick nvarchar2(64)                   , -- 别名
+carrierShortName nvarchar2(64)              , -- 简称
+officialWebSite nvarchar2(256)              , -- 官网
+hotline  nvarchar2(64)                      , -- 客服电话
+carrierAddTime timestamp default sysdate      -- 添加时间
 );
 
+comment on table  "carrierT"                   is '运营商表'    ;
+comment on column "carrierT"."carrierId"         is '运营商编号'  ;
+comment on column "carrierT"."carrierName"       is '运营商名'    ;
+comment on column "carrierT".carrierNick       is '别名'        ;
+comment on column "carrierT".carrierShortName  is '简称'        ;
+comment on column "carrierT".officialWebSite   is '官网'        ;
+comment on column "carrierT".hotline           is '客服电话'    ;
+comment on column "carrierT".carrierAddTime    is '添加时间'    ;
 
-insert into carrierT(carrierId,carrierName,carrierNick,carrierShortName,officialWebSite,hotline) values('carrier0000000000000000000000001','远特通信','远特','远特','http://www.yuantel.com/','4006091100');
-insert into carrierT(carrierId,carrierName,carrierNick,carrierShortName,officialWebSite,hotline) values('carrier0000000000000000000000002','国美极信通信','极信通信','国美','http://www.g-mobile.com.cn/','10037');
-insert into carrierT(carrierId,carrierName,carrierNick,carrierShortName,officialWebSite,hotline) values('carrier0000000000000000000000003','迪信通','迪迦通信','迪加','http://www.10026.cn/','4001610026');
-insert into carrierT(carrierId,carrierName,carrierNick,carrierShortName,officialWebSite,hotline) values('carrier0000000000000000000000004','蜗牛移动','蜗牛','蜗牛','http://10040.snail.com/pc/','10040');
+insert into "carrierT"("carrierId","carrierName",carrierNick,carrierShortName,officialWebSite,hotline) values('carrier0000000000000000000000001','远特通信','远特','远特','http://www.yuantel.com/','4006091100');
+insert into "carrierT"("carrierId","carrierName",carrierNick,carrierShortName,officialWebSite,hotline) values('carrier0000000000000000000000002','国美极信通信','极信通信','国美','http://www.g-mobile.com.cn/','10037');
+insert into "carrierT"("carrierId","carrierName",carrierNick,carrierShortName,officialWebSite,hotline) values('carrier0000000000000000000000003','迪信通','迪迦通信','迪加','http://www.10026.cn/','4001610026');
+insert into "carrierT"("carrierId","carrierName",carrierNick,carrierShortName,officialWebSite,hotline) values('carrier0000000000000000000000004','蜗牛移动','蜗牛','蜗牛','http://10040.snail.com/pc/','10040');
 
 
-select * from carrierT;
+select * from "carrierT";
 
 /********+*********+*********+*********+*********+*********+*/
--- documents/sqlscript/initTableSpaceAndUse.sql
+++ documents/sqlscript/initTableSpaceAndUse.sql
@@ -4,16 +4,16 @@
 *201704261019
 */
 
-create temporary tablespace simcustc1_temp 
-  tempfile 'D:\oracle12c\oradata\ORCL\simcustc1_temp.dbf' 
+create temporary tablespace baseoa_temp 
+  tempfile 'F:\oracledata\baseoa_temp.dbf' 
   size 30M 
   autoextend on 
   next 10M 
   extent management local;
   
- create tablespace simcustc1_data 
+ create tablespace baseoa_data 
   logging 
-  datafile 'D:\oracle12c\oradata\ORCL\simcustc1_data.dbf' 
+  datafile 'F:\oracledata\baseoa_data.dbf' 
   size 150M 
   autoextend on 
   next 10M 
